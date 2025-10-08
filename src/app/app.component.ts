b32c873f37b84cb33e009bd05ec3e626d8032a7f
rin-ormara <rinormara4@gmail.com>
2025-10-09 03:56:57 +0700

    ormara commit line of code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NavigationComponent } from "./pages/navigation/navigation.component";
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, FormsModule, NavigationComponent],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,27 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { FileInputComponent } from './pages/file-input/file-input.component';
+import { FileOutputComponent } from './pages/file-output/file-output.component';
+import { ReportComponent } from './pages/report/report.component';
 
 export const routes: Routes = [
 {
   path:'',
   pathMatch:'full',
-  
+  redirectTo:'fileInput'
+},
+{
+  path:'fileInput',
+  component:FileInputComponent
+},
+{
+  path:'fileOutput',
+  component:FileOutputComponent
+},
+{
+  path:'report',
+  component:ReportComponent
 }
-
 
 
 
