f856c1a4067c29164e91be6c21c72390fcac9601
e^ln(VatteyHeng <tey0996@gmail.com>
2025-10-11 09:40:49 +0700

    add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { FileInputComponent } from './pages/file-input/file-input.component';
 import { FileOutputComponent } from './pages/file-output/file-output.component';
 import { ReportComponent } from './pages/report/report.component';
+import { LoginComponent } from './pages/login/login.component';
+
 
 export const routes: Routes = [
 {
   path:'',
   pathMatch:'full',
-  redirectTo:'fileInput'
+  redirectTo: 'login'
+},
+{
+path:'login',
+component:LoginComponent
+
 },
 {
   path:'fileInput',
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RedirectFunction } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+ // static ROUTE: string | RedirectFunction | undefined;
+
+}
